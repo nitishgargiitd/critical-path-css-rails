2928e6ae6cf51a66327f5dd2d18ed13b08b8fcc7
nitishgargiitd <nitishgargiitd@gmail.com>
2017-05-24 17:50:12 -0400

    Update critical-path-css-rails.rb

--- lib/critical-path-css-rails.rb
+++ lib/critical-path-css-rails.rb
@@ -2,6 +2,10 @@ module CriticalPathCss
   require 'critical_path_css/css_fetcher'
 
   CACHE_NAMESPACE = 'critical-path-css'
+  
+  def self.cache_key(controller, action, asset_digest)
+    controller + "#" + action + "#" + asset_digest
+  end
 
   def self.generate(route)
     Rails.cache.write(
@@ -12,13 +16,15 @@ module CriticalPathCss
     )
   end
   
-  def self.generate_for_controller_action(controller, action, route)
+  def self.generate_for_controller_action(controller, action, asset_digest, route)
+    cache_key = CriticalPathCss.cache_key(controller, action, asset_digest)
     Rails.cache.write(
-      controller + "#" + action,
+      cache_key,
       CssFetcher.new.fetch_route(route),
       namespace: CACHE_NAMESPACE,
-      expires_in: nil
+      expires_in: 7.days
     )
+    $redis.del(cache_key)
   end
 
   def self.generate_all
@@ -39,7 +45,8 @@ module CriticalPathCss
     Rails.cache.read(route, namespace: CACHE_NAMESPACE) || ''
   end
   
-  def self.fetch_for_controller_action(controller, action)
-    Rails.cache.read(controller + "#" + action, namespace: CACHE_NAMESPACE) || ''
+  def self.fetch_for_controller_action(controller, action, asset_digest)
+    cache_key = CriticalPathCss.cache_key(controller, action, asset_digest)
+    Rails.cache.read(cache_key, namespace: CACHE_NAMESPACE) || ''
   end
 end
